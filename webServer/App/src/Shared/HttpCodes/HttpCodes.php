3ba199e5a0c4623a95369e7b732dfbac54d5cbfd
David Espinosa <despinosa1984@gmail.com>
2023-03-12 01:04:03 +0100

    Updated http codes with accepted

--- webServer/App/src/Shared/HttpCodes/HttpCodes.php
+++ webServer/App/src/Shared/HttpCodes/HttpCodes.php
@@ -5,10 +5,9 @@ namespace App\Shared\HttpCodes;
 class HttpCodes
 {
     public const SUCCESS = 200;
-    public const CREATED = 201;
-
     public const BAD_REQUEST = 400;
     public const NOT_FOUND = 404;
     public const SERVER_SIDE_ERROR = 500;
     public const CONFLICT = 409;
+    public const ACCEPTED = 202;
 }
